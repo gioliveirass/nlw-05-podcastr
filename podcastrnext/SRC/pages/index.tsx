eaba6fe4b4c6beb6fceed7d28596d7d929bdd882
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-04-20 15:59:38 -0300

    Consumindo API na home
    Faz a requisição SSG.

--- podcastrnext/SRC/pages/index.tsx
+++ podcastrnext/SRC/pages/index.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <h1>Index</h1>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return { 
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8, // A cada 8 horas (60*60*8) quando a página for acessada uma nova chamada API vai ser feita
+  }
 }
